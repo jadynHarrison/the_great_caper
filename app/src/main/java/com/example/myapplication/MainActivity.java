638f90181bba5bfa322354f8389a9d81182102f6
kendra <esquiall29@up.edu>
2026-02-01 12:48:41 -0800

    main_screen updated + player_select_screen made so it directs them to a separate page that ask how many people are playing

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.NumberPicker;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ImageButton playButton = findViewById(R.id.playButton);
+
+        playButton.setOnClickListener(v ->
+        {
+            Intent intent = new Intent(MainActivity.this, PlayerSelectActivity.class);
+            startActivity(intent);
+        });
+
+
+
     }
 }
